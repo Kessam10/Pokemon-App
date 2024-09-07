6b99cd346492b097585521ff4881a518544810d6
kareem <kareemessam878@gmail.com>
2024-09-07 15:15:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pokemonapp/CardItems.kt
@@ -0,0 +1,11 @@
+package com.example.pokemonapp
+
+import android.media.Image
+
+data class CardItems(
+val name: String?,
+val type: String?,
+val spAttack: Int?,
+val spDefense: Int?,
+val icon: String?
+)
